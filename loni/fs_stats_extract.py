c0a99d3cec179d98ddc4650429f4deaf71684b15
alexhanganu <alexandru@hanganu.ca>
2020-06-09 10:08:02 -0400

    m

--- loni/fs_stats_extract.py
+++ loni/fs_stats_extract.py
@@ -7,10 +7,13 @@ after that is necessary to zip the processed_fs_stats and move it to the corresp
 storage folder (i.e., for ADNI is beluga../projects/../adni)
 '''
 
-pproj = '/home/hanganua/projects/def-hanganua'
-pscratch = '/scratch/hanganua'
-path_processed = pproj+'/subjects_processed'
-path_stats = pscratch+'/processed_fs_stats'
+path_proj = '/home/hanganua/projects/def-hanganua'
+path_scratch = '/scratch/hanganua'
+path_processed = path_proj+'/subjects_processed'
+
+dir_stats = 'processed_fs_stats'
+path_stats = path_scratch+'/'+dir_stats
+
 
 from os import listdir, system, mkdir, path, chdir
 import shutil
@@ -30,3 +33,5 @@ for subDIR in listdir(path_processed):
     print('archiving ',subDIR)
     system('zip -q -r -m '+subDIR+'.zip '+subDIR)
 
+chdir(path_scratch)
+    system('zip -q -r -m '+dir_stats+'_20200609.zip '+dir_stats)
